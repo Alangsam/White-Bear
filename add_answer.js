7a5eedf41bf52e34359d80f8b2fdf107c5ff5b2c
Alangsam <alex@Alexs-MacBook-Pro.local>
2020-05-19 16:07:21 -0700

    maybe

--- add_answer.js
+++ add_answer.js
@@ -502,7 +502,7 @@ $(document).ready(function () {
          }, []);
          return String.fromCharCode(...newString);
       }
-      //i hope this
+      //i hope
       //console.log an object
       let createData = {
          _id: 678123,
